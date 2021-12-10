d816cb43783e6d6b96361eb561f23a0f3d464c09
Anton Matiunin <a.matiunin@studytube.nl>
2021-12-10 15:41:41 +0200

    last fixes

--- lib/anton_matiunin/bubble_sort.rb
+++ lib/anton_matiunin/bubble_sort.rb
@@ -7,14 +7,14 @@ module AntonMatiunin
     end
 
     def bubble_sort
-      a=input_array.length-1
-      input_array[0..a].each do
-        (0..input_array.length - 2).each do |i|
+      a = input_array.length - 2
+      input_array[0..-2].each do
+        (0..a).each do |i|
           if input_array[i] > input_array[i + 1]
             input_array[i], input_array[i + 1] = input_array[i + 1], input_array[i]
           end
-          a-=1
         end
+        a -= 1
       end
 
       input_array
--- lib/anton_matiunin/sub_strings.rb
+++ lib/anton_matiunin/sub_strings.rb
@@ -1,6 +1,6 @@
 module AntonMatiunin
   class SubStrings
-    attr_reader :input_string, :dictionary, :hash
+    attr_reader :input_string, :dictionary
 
     def initialize(input_string: '', dictionary: [])
       @input_string = input_string.to_s.downcase
@@ -8,9 +8,9 @@ module AntonMatiunin
     end
 
     def counter
-      dictionary.each_with_object({}) do |word, hash|
+      dictionary.each_with_object({}) do |word, words_hash|
         founded_words = input_string.scan(word).count
-        hash[word] = founded_words if founded_words.positive?
+        words_hash[word] = founded_words if founded_words.positive?
       end
     end
   end
